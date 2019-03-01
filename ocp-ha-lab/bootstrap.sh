1f574b9d89715d0606601322df7bac7fb746946b
Alan Chan <achanwl@gmail.com>
2019-03-01 18:25:49 -0500

    Initial commit

---
+++ ocp-ha-lab/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+# Pre-install
+#
+# Needs to be separated cuz it creates inventory file
+#
+ansible-playbook -f 10 --ask-vault-pass playbooks/preinstall.yml
+
+# Build
+#
+ansible-playbook -f 10 --ask-vault-pass playbooks/build.yml
