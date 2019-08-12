a2c69cfcf413b6d3179c22c54799d81d52e106ca
Alan Chan <achanwl@gmail.com>
2019-08-12 11:29:12 -0400

    Removed -f 20

--- ocp-ha-lab/bootstrap.sh
+++ ocp-ha-lab/bootstrap.sh
@@ -5,8 +5,8 @@
 #
 # Needs to be separated cuz it creates inventory file
 #
-ansible-playbook -f 20 --ask-vault-pass playbooks/preinstall.yml
+ansible-playbook --ask-vault-pass playbooks/preinstall.yml
 
 # Build
 #
-ansible-playbook -f 20 --ask-vault-pass playbooks/build.yml
+ansible-playbook --ask-vault-pass playbooks/build.yml
