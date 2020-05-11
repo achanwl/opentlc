87ddd80fb07f86db6b927732e3ef43599cb430b7
Alan Chan <achanwl@gmail.com>
2020-05-10 23:57:49 -0400

    Added separate bastion handling

--- ocp-ha-lab/bootstrap.sh
+++ ocp-ha-lab/bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 
+# Update & reboot bastion first
+#
+ansible-playbook playbooks/update-bastion.yml
+
 # Pre-install
 #
 # Needs to be separated cuz it creates inventory file
 #
 ansible-playbook -f 10 --ask-vault-pass playbooks/preinstall.yml
-
-# Reboot bastion
-#
-reboot
 
 # Build
 #
